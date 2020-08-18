e9648ba8f9572a79971010ad0abe723bda4b3745
Wim Selles <wim@wswebcreation.nl>
2020-08-18 14:36:17 +0200

    test: add w3c/jsonwp config

--- v6/test/configs/web/wdio.sauce.sim.browser.ios.conf.js
+++ v6/test/configs/web/wdio.sauce.sim.browser.ios.conf.js
@@ -1,11 +1,11 @@
-const { config } = require('../wdio.sauce.shared.conf');
+const {config} = require('../wdio.sauce.shared.conf');
 const build = `Sauce Simulator browser iOS - ${new Date().getTime()}`;
 
 // ==================
 // Specify Test Files
 // ==================
-config.specs= [
-    './test/web/specs/**/*.js',
+config.specs = [
+    './test/web/specs/demo-site/best-practices/login.spec.js',
 ];
 
 // ============
@@ -21,18 +21,45 @@ config.capabilities = [
         platformVersion: '13.2',
         build,
     },
-    // W3C
+    // W3C working
     {
         platformName: 'iOS',
         browserName: 'safari',
         'appium:deviceName': 'iPhone Simulator',
         'appium:platformVersion': '13.2',
-        'appium:deviceOrientation': 'portrait',
+        'appium:orientation': 'portrait',
         'sauce:options': {
             'appiumVersion': '1.16.0',
             build,
         }
     },
+    // W3C according to docs, https://wiki.saucelabs.com/display/DOCS/Test+Configuration+Options
+    // NOT WORKING!!
+    {
+        'appium:platformName': 'iOS',
+        'appium:browserName': 'safari',
+        'appium:deviceName': 'iPhone Simulator',
+        'appium:platformVersion': '13.2',
+        'appium:orientation': 'portrait',
+        'sauce:options': {
+            'appiumVersion': '1.16.0',
+            build,
+        }
+    },
+    // W3C, NOT WORKING for EMUSIM and OPENS A UP INSTANCE ON A REAL DEVICE
+    {
+        platformName: 'iOS',
+        browserName: 'safari',
+        appium: {
+            deviceName: 'iPhone Simulator',
+            platformVersion: '13.2',
+            orientation: 'portrait',
+        },
+        'sauce:options': {
+            'appiumVersion': '1.16.0',
+            build,
+        }
+    }
 ];
 
 exports.config = config;
